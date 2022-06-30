694b4147bd8fb00c90b47812a3db9b037f5286fa
Stefano Butera <stefano.butera@nethnic.com>
2022-06-30 19:51:17 +0100

    Extract menus from main markup of toolbar

--- src/components/Editor/Toolbar/Toolbar.tsx
+++ src/components/Editor/Toolbar/Toolbar.tsx
@@ -27,68 +27,74 @@ const Toolbar = (props) => {
         return '';
     };
 
+    const primaryMenu = () => {
+        return Object.keys(props.menus)
+            .map((item, key) => {
+                const current = props.menus[item];
+
+                return (
+                    <div
+                        key={key}
+                        className={`Item ${(item == primary) ? 'Selected' : ''}`}
+                        onClick={(e) => {
+                            selectPrimaryItem(item)(e);
+                        }}
+                        >
+                        {icon(current)}{item}
+                    </div>
+                );
+            });
+    };
+
+    const secondaryMenu = () => {
+        if (props.menus[primary].component) {
+            return props.menus[primary].component;
+        }
+
+        if (props.menus[primary].submenus) {
+            return Object.keys(props.menus[primary].submenus)
+                .map((item, key) => {
+                    const current = props.menus[primary].submenus[item];
+
+                    return (
+                        <div
+                            key={primary + '_' + key}
+                            className={`Item ${current.submenus ? 'Submenu' : ''} ${(item == secondary) ? 'Selected' : ''} ${current.active ? 'Active' : ''}`}
+                            onClick={(e) => {
+                                selectSecondaryItem(item !== secondary ? item : null)(e);
+                                if (current.execute) current.execute(e.target);
+                            }}
+                            >
+                            {icon(current)}{item}
+                        </div>
+                    );
+                });
+        }
+    };
+
+    const tertiaryMenu = () => {
+        if (!hasTertiary) return null;
+
+        return Object.keys(props.menus[primary].submenus[secondary].submenus)
+            .map((item, key) => {
+                const current = props.menus[primary].submenus[secondary].submenus[item];
+
+                return (
+                    <div
+                        key={primary + '_' + secondary + '_' + key}
+                        className={`Item ${current?.active ? 'Active' : ''}`}
+                        onClick={(e) => current.execute(e.target)}
+                        >
+                        {icon(current)}{item}
+                    </div>
+                );
+            });
+    };
+
     return <div className="Toolbar">
-        <div className="Menu Primary">
-            {
-                Object.keys(props.menus)
-                    .map((item, key) => {
-                        const current = props.menus[item];
-
-                        return (
-                            <div
-                                key={key}
-                                className={`Item ${(item == primary) ? 'Selected' : ''}`}
-                                onClick={(e) => {
-                                    selectPrimaryItem(item)(e);
-                                }}
-                                >
-                                {icon(current)}{item}
-                            </div>
-                        );
-                    })
-            }
-        </div>
-        <div className="Menu Secondary">
-            {
-                Object.keys(props.menus[primary].submenus)
-                    .map((item, key) => {
-                        const current = props.menus[primary].submenus[item];
-
-                        return (
-                            <div
-                                key={primary + '_' + key}
-                                className={`Item ${current.submenus ? 'Submenu' : ''} ${(item == secondary) ? 'Selected' : ''} ${current.active ? 'Active' : ''}`}
-                                onClick={(e) => {
-                                    selectSecondaryItem(item !== secondary ? item : null)(e);
-                                    if (current.execute) current.execute(e.target);
-                                }}
-                                >
-                                {icon(current)}{item}
-                            </div>
-                        );
-                    })
-            }
-        </div>
-        <div className={`Menu Tertiary ${secondary} ${hasTertiary ? '' : 'Hidden'}`}>
-            {
-                hasTertiary
-                    ? Object.keys(props.menus[primary].submenus[secondary].submenus)
-                        .map((item, key) => {
-                            const current = props.menus[primary].submenus[secondary].submenus[item];
-
-                            return (
-                                <div
-                                    key={primary + '_' + secondary + '_' + key}
-                                    className={`Item ${current?.active ? 'Active' : ''}`}
-                                    onClick={(e) => current.execute(e.target)}
-                                    >
-                                    {icon(current)}{item}
-                                </div>
-                            );
-                        })
-                    : null
-            }
-        </div>
+        <div className="Menu Primary">{primaryMenu()}</div>
+        <div className="Menu Secondary">{secondaryMenu()}</div>
+        <div className={`Menu Tertiary ${secondary ?? ''} ${hasTertiary ? '' : 'Hidden'}`}>{tertiaryMenu()}</div>
     </div>;
 };
 
