6d66f1d6c6af3949a2dd10b8341f4415f7e68b84
Stefano Butera <stefano.butera@nethnic.com>
2022-07-20 18:47:48 +0100

    Add Threshold panel

--- src/components/panels/Functions/index.tsx
+++ src/components/panels/Functions/index.tsx
@@ -5,9 +5,12 @@ import Multiply from './multiply';
 import Divide from './divide';
 import Invert from './invert';
 
+import Threshold from './threshold';
+
 import And from './and';
 import Or from './or';
 import Not from './not';
+
 
 export default {
     Sum,
@@ -16,6 +19,7 @@ export default {
     Multiply,
     Divide,
     Invert,
+    Threshold,
     And,
     Or,
     Not
---
+++ src/components/panels/Functions/threshold.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+
+import { Panel } from '../../../types/Panel';
+
+import InputEndpoint from '../../Editor/Panel/InputEndpoint';
+import OutputEndpoint from '../../Editor/Panel/OutputEndpoint';
+
+const create = (panelId: number): Panel => {
+    const Component = (props) => {
+        return <>
+            <div className="Row">
+                <InputEndpoint name="Threshold" panelId={panelId} {...props}>Threshold</InputEndpoint>
+                <OutputEndpoint name="High" panelId={panelId} {...props}>High</OutputEndpoint>
+            </div>
+            <div className="Row">
+                <InputEndpoint name="Value" panelId={panelId} {...props}>Value</InputEndpoint>
+                <OutputEndpoint name="Equal" panelId={panelId} {...props}>Equal</OutputEndpoint>
+            </div>
+            <div className="Row">
+                <OutputEndpoint name="Low" panelId={panelId} {...props}>Low</OutputEndpoint>
+            </div>
+        </>;
+    };
+
+    const inputEndpoints = [{
+        name: 'Threshold',
+        defaultValue: 0,
+        signal: 'Value'
+    }, {
+        name: 'Value',
+        defaultValue: 0,
+        signal: 'Value'
+    }];
+
+    const outputEndpoints = [{
+        name: 'High',
+        default: false,
+        signal: 'Value'
+    }, {
+        name: 'Equal',
+        default: false,
+        signal: 'Value'
+    }, {
+        name: 'Low',
+        default: false,
+        signal: 'Value'
+    }];
+
+    const execute = (panel, values) => {
+        return {
+            outputHigh: parseFloat(values.inputValue) > parseFloat(values.inputThreshold),
+            outputEqual: parseFloat(values.inputValue) == parseFloat(values.inputThreshold),
+            outputLow: parseFloat(values.inputValue) < parseFloat(values.inputThreshold)
+        };
+    };
+
+    return {
+        type: 'Threshold',
+        starter: true,
+        inputEndpoints,
+        outputEndpoints,
+        width: 134,
+        height: 94,
+        Component,
+        execute
+    } as Panel;
+};
+
+export default {
+    create
+};
