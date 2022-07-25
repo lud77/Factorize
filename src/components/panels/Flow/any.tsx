d503b8379ce45f35add7fb83a6b32c3787603c36
Stefano Butera <stefano.butera@nethnic.com>
2022-07-25 12:06:47 +0100

    Add Any panel

---
+++ src/components/panels/Flow/any.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+
+import { Panel } from '../../../types/Panel';
+
+import InputEndpoint from '../../Editor/Panel/InputEndpoint';
+import OutputEndpoint from '../../Editor/Panel/OutputEndpoint';
+
+const panelType = 'Any';
+
+const create = (panelId: number): Panel => {
+    const handleClick = ({ panel, machine }) => (e) => {
+        machine.addInputEndpoint(
+            panelId,
+            `In`,
+            `In${panel.inEpsCounter}`,
+            0,
+            'Pulse',
+            0,
+            'inEps'
+        );
+    };
+
+    const Component = (props) => {
+        return <>
+            <div className="Row">
+                <div className="InteractiveItem">
+                    <button onClick={handleClick(props)}>+</button>
+                </div>
+            </div>
+            <div className="Row">
+                <InputEndpoint name="In1" panelId={panelId} signal="Pulse" {...props}>In</InputEndpoint>
+                <OutputEndpoint name="Out" panelId={panelId} signal="Pulse" {...props}>Out</OutputEndpoint>
+            </div>
+            <div className="Row">
+                <InputEndpoint name="In2" panelId={panelId} signal="Pulse" {...props}>In</InputEndpoint>
+            </div>
+            {
+                props.panel.inEps.map(([ep, epRef, label, name], key) => (
+                    <div className="Row" key={key}>
+                        <InputEndpoint name={name} panelId={panelId} signal="Pulse" removable={true} registry="inEps" {...props}>{label}</InputEndpoint>
+                    </div>
+                ))
+            }
+        </>;
+    };
+
+    const inputEndpoints = [{
+        name: 'In1',
+        signal: 'Pulse'
+    }, {
+        name: 'In2',
+        signal: 'Pulse'
+    }];
+
+    const outputEndpoints = [{
+        name: 'Out',
+        signal: 'Pulse'
+    }];
+
+    const onPulse = (ep, panel, sendPulseTo) => {
+        switch (ep) {
+            default:
+                sendPulseTo(panel.panelId, 'outputOut');
+                return {};
+        }
+    };
+
+    const execute = (panel, values) => values;
+
+    return {
+        type: panelType,
+        starter: true,
+        inputEndpoints,
+        outputEndpoints,
+        width: 134,
+        height: 94,
+        inEps: [],
+        inEpsCounter: 3,
+        Component,
+        execute,
+        onPulse
+    } as Panel;
+};
+
+export default {
+    type: panelType,
+    create
+};
--- src/components/panels/Flow/index.tsx
+++ src/components/panels/Flow/index.tsx
@@ -2,10 +2,12 @@ import Fan from './fan';
 import Memory from './memory';
 import If from './if';
 import Delta from './delta';
+import Any from './any';
 
 export default {
     Fan,
     Memory,
     If,
-    Delta
+    Delta,
+    Any
 };
