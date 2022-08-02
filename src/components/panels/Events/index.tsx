154e6c48aa626a18c3e2b396931722d04c36e921
Stefano Butera <stefano.butera@nethnic.com>
2022-08-02 18:44:47 +0100

    Move delta panel from Flow palette to Events palette

--- src/components/panels/Events/index.tsx
+++ src/components/panels/Events/index.tsx
@@ -1,7 +1,9 @@
 import Button from './button';
 import Clock from './clock';
+import Delta from '../Events/delta';
 
 export default {
     Button,
-    Clock
+    Clock,
+    Delta
 };
--- src/components/panels/Flow/index.tsx
+++ src/components/panels/Flow/index.tsx
@@ -1,7 +1,6 @@
 import Fan from './fan';
 import Memory from './memory';
 import If from './if';
-import Delta from './delta';
 import Any from './any';
 import Throttle from './throttle';
 
@@ -9,7 +8,6 @@ export default {
     Fan,
     Memory,
     If,
-    Delta,
     Any,
     Throttle
 };
