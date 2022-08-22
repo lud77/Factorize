277b6c1405879338b0b0d3cafc71aab374c4f9b3
Stefano Butera <stefano.butera@nethnic.com>
2022-08-22 15:55:19 +0100

    Simplify PushButton component

--- src/components/PushButton/PushButton.tsx
+++ src/components/PushButton/PushButton.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import './PushButton.css';
 
-const PushButton = (props) => {
-    return (
-        <button className="PushButton" onClick={props.onClick}>{props.children}</button>
-    );
-};
+const PushButton = (props) => (<button className="PushButton" onClick={props.onClick}>{props.children}</button>);
 
 export default PushButton;
