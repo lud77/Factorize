68b12b9a2cf862f9658943ee8a58e4908f1dc129
Stefano Butera <stefano.butera@nethnic.com>
2022-08-29 17:26:07 +0100

    Add Seed and Reset endpoints to Random panel

--- src/components/panels/Sources/random.tsx
+++ src/components/panels/Sources/random.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Random from 'random-seed';
 
 import { Panel } from '../../../types/Panel';
 
@@ -7,8 +8,11 @@ import OutputEndpoint from '../../Editor/Panel/OutputEndpoint';
 
 const panelType = 'Random';
 
+const defaultSeed = Math.random();
+let rand = Random(defaultSeed);
+
 const create = (panelId: number): Panel => {
-    const getValue = () => Math.random();
+    const getValue = () => rand.random();
 
     const Component = (props) => {
         return <>
@@ -16,17 +20,31 @@ const create = (panelId: number): Panel => {
                 <InputEndpoint name="Fetch" panelId={panelId} signal="Pulse" description="Produce a random number" {...props}>Fetch</InputEndpoint>
                 <OutputEndpoint name="Value" panelId={panelId} {...props}>Value</OutputEndpoint>
             </div>
+            <div className="Row">
+                <InputEndpoint name="Seed" panelId={panelId} signal="Value" editable={true} {...props}>Seed</InputEndpoint>
+            </div>
+            <div className="Row">
+                <InputEndpoint name="Reset" panelId={panelId} signal="Pulse" description="Produce a random number" {...props}>Reset</InputEndpoint>
+            </div>
         </>;
     };
 
     const inputEndpoints = [{
         name: 'Fetch',
         signal: 'Pulse'
+    }, {
+        name: 'Seed',
+        defaultValue: defaultSeed,
+        type: 'number',
+        signal: 'Value'
+    }, {
+        name: 'Reset',
+        signal: 'Pulse'
     }];
 
     const outputEndpoints = [{
         name: 'Value',
-        defaultValue: getValue(),
+        defaultValue: '',
         type: 'number',
         signal: 'Value'
     }];
@@ -35,10 +53,28 @@ const create = (panelId: number): Panel => {
         switch (ep) {
             case 'inputFetch':
                 return { outputValue: getValue() };
+
+            case 'inputReset':
+                rand = Random(panel.inputEpValues.inputSeed);
+                return { outputValue: '' };
         }
     };
 
-    const execute = (panel, values) => values;
+    const execute = (panel, values) => {
+        console.log('random execute');
+        if ((panel.outputEpValues.oldSeed == null) || (panel.inputEpValues.inputSeed != panel.outputEpValues.oldSeed)) {
+            console.log('random execute - create instance');
+            rand = Random(panel.inputEpValues.inputSeed);
+
+            return {
+                ...values,
+                oldSeed: panel.inputEpValues.inputSeed,
+                outputValue: ''
+            };
+        }
+
+        return values;
+    };
 
     return {
         type: panelType,
@@ -46,7 +82,7 @@ const create = (panelId: number): Panel => {
         inputEndpoints,
         outputEndpoints,
         width: 134,
-        height: 54,
+        height: 93,
         Component,
         execute,
         onPulse
