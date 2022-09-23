1825195e301d679d815b380d370fe696c9b02908
Stefano Butera <stefano.butera@nethnic.com>
2022-09-23 15:56:25 +0100

    Small variation to background checkers pattern

--- src/components/panels/Outputs/imageView.tsx
+++ src/components/panels/Outputs/imageView.tsx
@@ -30,6 +30,7 @@ const create = (panelId: number): Panel => {
 
         const backgroundStyle = {
             backgroundImage: 'linear-gradient(45deg, #808080 25%, transparent 25%), linear-gradient(-45deg, #808080 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #808080 75%), linear-gradient(-45deg, transparent 75%, #808080 75%)',
+            backgroundColor: '#ccc',
             backgroundSize: '20px 20px',
             backgroundPosition: '0 0, 0 10px, 10px -10px, -10px 0px',
             overflow: 'hidden',
