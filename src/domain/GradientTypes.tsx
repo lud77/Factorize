0a7d76c21de4b3201455121591099c37931fba2c
Stefano Butera <stefano.butera@nethnic.com>
2022-10-09 00:12:16 +0100

    Add math for circular gradient

--- src/domain/GradientTypes.tsx
+++ src/domain/GradientTypes.tsx
@@ -18,8 +18,30 @@ const linear = (offsetX, offsetY, angle, length, gradient) => {
     }
 };
 
+const circular = (offsetX, offsetY, angle, length, gradient) => {
+    const Rad = Math.PI / 180;
+    const cosa = Math.cos(angle * Rad);
+    const sina = Math.sin(angle * Rad);
+
+    const getColorAt = renderGradient(gradient, length);
+
+    return (x, y) => {
+        const ox = x - offsetX;
+        const oy = y - offsetY;
+
+        const ax = cosa * ox - sina * oy;
+        const ay = sina * ox + cosa * oy;
+
+        const distValue = Math.sqrt(ax * ax + ay * ay);
+        const value = Math.abs(100 * Math.sin(distValue * Rad));
+
+        return getColorAt(value);
+    }
+};
+
 const GradientTypes = {
-    'Linear': linear
+    'Linear': linear,
+    'Circular': circular
 };
 
 export default GradientTypes;
