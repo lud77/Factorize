8b2b9ffe50492a0e8bb7396fd4088fd8f513439c
Stefano Butera <stefano.butera@nethnic.com>
2023-12-16 16:47:33 +0000

    Add tests for counter panel component

---
+++ src/components/panels/Events/counter.test.tsx
@@ -0,0 +1,21 @@
+import { fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import setupPanel from '../utils/panelTestUtils';
+import PanelBundle from './counter';
+
+describe('Counter panel component', () => {
+    it('should be rendered', () => {
+        const { renderer } = setupPanel(PanelBundle);
+        const { getByText } = renderer;
+
+        const eventInput = getByText('Event');
+        expect(eventInput).toBeInTheDocument();
+
+        const resetInput = getByText('Reset');
+        expect(resetInput).toBeInTheDocument();
+
+        const countOutput = getByText('Count');
+        expect(countOutput).toBeInTheDocument();
+    });
+});
